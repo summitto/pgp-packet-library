fbf613668689414df089764b794d1fbb2b7eb424
Martijn Otto <git@martijnotto.nl>
2018-12-17 14:09:38 +0100

    Add utility function for dumping tag type description

--- packet_tag.h
+++ packet_tag.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <boost/utility/string_view.hpp>
 
 
 namespace pgp {
@@ -29,6 +30,37 @@ namespace pgp {
         symmetrically_encrypted_and_integrity_protected_data    = 18,
         modification_detection_code                             = 19
     };
+
+    /**
+     *  Get a description of the packet tag
+     *
+     *  @param  tag     The packet tag to get a description for
+     *  @return The packet tag to describe
+     */
+    constexpr boost::string_view packet_tag_description(packet_tag tag) noexcept
+    {
+        // check the provided tag
+        switch (tag) {
+            case packet_tag::reserved:                                              return "reserved tag - do not use";
+            case packet_tag::public_key_encrypted_session_key:                      return "public key encrypted session key packet";
+            case packet_tag::signature:                                             return "signature packet";
+            case packet_tag::symmetric_key_encrypted_session_key:                   return "symmetric key encrypted session key packet";
+            case packet_tag::one_pass_signature:                                    return "one pass signature packet";
+            case packet_tag::secret_key:                                            return "secret key packet";
+            case packet_tag::public_key:                                            return "public key packet";
+            case packet_tag::secret_subkey:                                         return "secret subkey packet";
+            case packet_tag::compressed_data:                                       return "compressed data packet";
+            case packet_tag::symmetrically_encrypted_data:                          return "symmetrically encrypted data packet";
+            case packet_tag::marker_packet:                                         return "marker packet";
+            case packet_tag::literal_data:                                          return "literal data packet";
+            case packet_tag::trust_packet:                                          return "trust packet";
+            case packet_tag::user_id:                                               return "user id packet";
+            case packet_tag::public_subkey:                                         return "public subkey packet";
+            case packet_tag::user_attribute:                                        return "user attribute packet";
+            case packet_tag::symmetrically_encrypted_and_integrity_protected_data:  return "symmetrically encrypted and integrity protected data packet";
+            case packet_tag::modification_detection_code:                           return "modification detection code";
+        };
+    }
 
     /**
      *  Check whether a packet tag is compatible with the
