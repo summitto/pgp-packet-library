e950d229a4088085adb6f3c20ef82fa75f146786
Martijn Otto <git@martijnotto.nl>
2019-07-18 15:59:28 +0200

    Use the correct span abstraction

--- include/decoder_traits.h
+++ include/decoder_traits.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include "util/span.h"
 #include <type_traits>
-#include <gsl/span>
 #include <cstddef>
 #include <cstdint>
 
@@ -21,13 +21,13 @@ namespace pgp {
     template<typename T>
     struct is_decoder<T, std::void_t<
         decltype(std::declval<T>().splice(0)),  // note: cannot check for a valid decoder return type, template recursion
-        std::enable_if_t<std::is_same_v<bool,                       decltype(std::declval<T>().empty())                             >>,
-        std::enable_if_t<std::is_same_v<size_t,                     decltype(std::declval<T>().size())                              >>,
-        std::enable_if_t<std::is_same_v<uint8_t,                    decltype(std::declval<T>().peek_bits(0))                        >>,
-        std::enable_if_t<std::is_same_v<uint8_t,                    decltype(std::declval<T>().extract_bits(0))                     >>,
-        std::enable_if_t<std::is_same_v<uint8_t,                    decltype(std::declval<T>().template peek_number<uint8_t>())     >>,
-        std::enable_if_t<std::is_same_v<uint8_t,                    decltype(std::declval<T>().template extract_number<uint8_t>())  >>,
-        std::enable_if_t<std::is_same_v<gsl::span<const uint8_t>,   decltype(std::declval<T>().template extract_blob<uint8_t>(0))   >>
+        std::enable_if_t<std::is_same_v<bool,                   decltype(std::declval<T>().empty())                             >>,
+        std::enable_if_t<std::is_same_v<size_t,                 decltype(std::declval<T>().size())                              >>,
+        std::enable_if_t<std::is_same_v<uint8_t,                decltype(std::declval<T>().peek_bits(0))                        >>,
+        std::enable_if_t<std::is_same_v<uint8_t,                decltype(std::declval<T>().extract_bits(0))                     >>,
+        std::enable_if_t<std::is_same_v<uint8_t,                decltype(std::declval<T>().template peek_number<uint8_t>())     >>,
+        std::enable_if_t<std::is_same_v<uint8_t,                decltype(std::declval<T>().template extract_number<uint8_t>())  >>,
+        std::enable_if_t<std::is_same_v<span<const uint8_t>,    decltype(std::declval<T>().template extract_blob<uint8_t>(0))   >>
     >> : std::true_type {};
 
     /**
