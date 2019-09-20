afcb26a2ac4bf02ebf7c5f98b7ced542f64a96ff
Martijn Otto <git@martijnotto.nl>
2019-09-20 17:13:47 +0200

    Improve includes for string_to_key.h

--- include/string_to_key.h
+++ include/string_to_key.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include "fixed_number.h"
+#include <cstdint>          // for uint8_t
+#include <cstddef>          // for size_t
+#include <type_traits>      // for enable_if_t
+#include "decoder_traits.h" // for is_decoder_v
+#include "fixed_number.h"   // for uint8
 
 
 namespace pgp {
