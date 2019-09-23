45379d701ab2835327a6470743a11bfbca9a35ed
Martijn Otto <git@martijnotto.nl>
2019-09-23 11:46:15 +0200

    Cleanup includes for elgamal_public_key.h

--- include/elgamal_public_key.h
+++ include/elgamal_public_key.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "multiprecision_integer.h"
-#include "packet_tag.h"
+#include <cstddef>                  // for size_t
+#include <type_traits>              // for enable_if_t
+#include "decoder_traits.h"         // for is_decoder_v
+#include "multiprecision_integer.h" // for multiprecision_integer
 
 
 namespace pgp {
